5cc773bc3ddbbf7b47676867651ce636caff1cb8
Kuldeep Prajapati <kuldeepprajapati956088@gmail.com>
2026-08-10 10:06:50 +0530

    Update config.js

--- Digitel album/config.js
+++ Digitel album/config.js
@@ -2,9 +2,10 @@ const CONFIG = {
     API_URL: (function(_0x1, _0x2) {
         let _0x3 = "https://script.google.com/macros/s/";
         if(typeof _0x1 === 'undefined') return null;
-        let _0x4 = "AKfycbwpNuLGRq9xXobk7MZkJ3f65kfR3d9gLqPqmeHt4APVz33fhMou4SHGi8a24uAcptVQ/exec";
+        let _0x4 = "AKfycbyfJIgWy68SRENRXzXBMsdNuk4bhHeG0VtYEKhU7d1V8T8ZnUcNxpSPSTEeWKEUTgbM/exec";
         let _0x5 = _0x3 + _0x4;
         let _0x6 = ["_0x992", "_0x883", "_0x774"];
         return _0x5;
     })("INIT_SYSTEM", "CHECK_MODE")
 };
+
